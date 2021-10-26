0e42b71a518a334652dfea99a532639c15b2189f
Crystal Chang <3077117+ACrystalC@users.noreply.github.com>
2021-10-26 16:44:02 -0400

    add auth provider

--- apps/guide/pages/_app.tsx
+++ apps/guide/pages/_app.tsx
@@ -2,6 +2,38 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
 
+import { Provider as AuthProvider } from 'next-auth/client';
+
+import { shouldPolyfill as shouldPolyfillIntlRelativeTimeFormat } from '@formatjs/intl-relativetimeformat/should-polyfill';
+import { shouldPolyfill as shouldPolyfillIntlDateTimeFormat } from '@formatjs/intl-datetimeformat/should-polyfill';
+
+/* --------------------------------------------------------
+ * Polyfills for Intl API
+ * ------------------------------------------------------*/
+
+(async () => {
+  const polyfills = [];
+  const data = [];
+
+  if (!shouldPolyfillIntlRelativeTimeFormat()) {
+    polyfills.push(import('@formatjs/intl-relativetimeformat/polyfill'));
+
+    data.push(import('@formatjs/intl-relativetimeformat/locale-data/en'));
+  }
+
+  if (!shouldPolyfillIntlDateTimeFormat()) {
+    polyfills.push(import('@formatjs/intl-datetimeformat/polyfill'));
+
+    data.push(import('@formatjs/intl-datetimeformat/add-golden-tz'));
+    data.push(import('@formatjs/intl-datetimeformat/locale-data/en'));
+  }
+
+  if (polyfills.length !== 0) {
+    await Promise.all(polyfills);
+    await Promise.all(data);
+  }
+})();
+
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -15,7 +47,12 @@ function CustomApp({ Component, pageProps }: AppProps) {
           <h1>Welcome to guide!</h1>
         </header>
         <main>
+        <AuthProvider
+          session={{}}
+          options={{}}
+        >
           <Component {...pageProps} />
+        </AuthProvider>
         </main>
       </div>
     </>
